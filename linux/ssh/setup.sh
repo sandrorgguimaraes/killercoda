e0c1f6920667cce3df80782fdad8a4d67bb3c239
Sandro Rogerio Galvao Guimaraes <sandrorgguimaraes@gmail.com>
2022-10-13 08:26:20 -0300

    Alterado o 'setup.sh'.

--- linux/ssh/setup.sh
+++ linux/ssh/setup.sh
@@ -1,9 +1,7 @@
-USUARIO="ubuntu1"
-SENHA="$(openssl passwd -1 $USUARIO)"
-CMD="$(cat <<EOF
-useradd -m -s /usr/bin/bash -p $SENHA $USUARIO;
-EOF
-)"
-useradd -m -s /usr/bin/bash -p $SENHA $USUARIO;
-ssh node01 $CMD;
+# USUARIO="ubuntu1"
+# SENHA="$(openssl passwd -1 $USUARIO)"
+# useradd -m -s /usr/bin/bash -p $SENHA $USUARIO
+# ssh node01 'useradd -m -s /usr/bin/bash -p $SENHA $USUARIO'
 
+useradd -m -s /usr/bin/bash -p $(openssl passwd -1 ubuntu1) ubuntu1
+ssh node01 'useradd -m -s /usr/bin/bash -p $(openssl passwd -1 ubuntu1) ubuntu1'
