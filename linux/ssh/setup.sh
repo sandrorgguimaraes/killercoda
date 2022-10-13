ff7d5800f1e7d2ede8b9f642b838d1ad4d4bce4e
Sandro Rogerio Galvao Guimaraes <sandrorgguimaraes@gmail.com>
2022-10-13 08:50:37 -0300

    Alteração final no 'setup.sh'.

--- linux/ssh/setup.sh
+++ linux/ssh/setup.sh
@@ -1,7 +1,2 @@
-# USUARIO="ubuntu1"
-# SENHA="$(openssl passwd -1 $USUARIO)"
-# useradd -m -s /usr/bin/bash -p $SENHA $USUARIO
-# ssh node01 'useradd -m -s /usr/bin/bash -p $SENHA $USUARIO'
-
 useradd -m -s /usr/bin/bash -p $(openssl passwd -1 ubuntu1) ubuntu1
 ssh node01 'useradd -m -s /usr/bin/bash -p $(openssl passwd -1 ubuntu1) ubuntu1'
