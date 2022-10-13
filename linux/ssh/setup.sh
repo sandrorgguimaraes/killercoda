34f5d029a8fdf3c9d0b6fa403479a2d4ee5fec56
Sandro Rogerio Galvao Guimaraes <sandrorgguimaraes@gmail.com>
2022-10-13 07:29:40 -0300

    Alterado o 'setup.sh'.

--- linux/ssh/setup.sh
+++ linux/ssh/setup.sh
@@ -1,5 +1,4 @@
 USUARIO="ubuntu1";
-SENHA="$(openssl passwd -1 $USUARIO)";
-echo "Criando o usuário 'ubuntu1'...";
-sudo useradd -m -s /usr/bin/bash -p "$SENHA" "$USUARIO";
-ssh node01 'sudo useradd -m -s /usr/bin/bash -p "$SENHA" "$USUARIO"';
+COMANDO="sudo useradd -m -s /usr/bin/bash -p $(openssl passwd -1 $USUARIO) $USUARIO";
+eval $COMANDO;
+ssh node01 $COMANDO;
